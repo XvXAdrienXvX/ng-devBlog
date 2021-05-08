9f895e21dad6c2bd8619322660196f9cd7a399a3
XvXAdrienXvX <you@example.com>
2021-05-08 13:10:44 +0400

    fixed css

--- src/app/Post/post-details/post-details.component.ts
+++ src/app/Post/post-details/post-details.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { BlogPostService } from 'src/app/Core/post.service';
 import { ActivatedRoute } from '@angular/router';
+import { ViewEncapsulation } from '@angular/core';
 
 @Component({
     selector : 'post-list',
-    templateUrl: './post-details.component.html'
+    templateUrl: './post-details.component.html',
+    styleUrls:[
+        '/post-details.component.css'
+    ],
+    encapsulation: ViewEncapsulation.None
 })
 export class PostDetailsComponent{
    post: any
